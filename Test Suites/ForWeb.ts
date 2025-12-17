7eacf36fb4614de87feeab0346585460b4a6e251
subhan.hassan@globalnorthstar.com <katalon_g3_dummy_username>
2025-12-17 09:23:06 +0000

    Create "ForWeb" test suite

---
+++ Test Suites/ForWeb.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>ForWeb</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>be991f92-c03c-4afc-9ad8-e51fc30a57b5</testSuiteGuid>
+    <testCaseLink>
+        <guid>e4767a33-f0e4-4481-92b4-d91beda68428</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Login/login</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+</TestSuiteEntity>
