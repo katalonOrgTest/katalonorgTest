4453dc1c7bf7a85e6cdbdd4bbeed246e84b64077
subhan.hassan@globalnorthstar.com <katalon_g3_dummy_username>
2025-12-17 10:55:41 +0000

    Edit "WebRunWithNasar" test suite

--- Test Suites/WebRunWithNasar.ts
+++ Test Suites/WebRunWithNasar.ts
@@ -1,21 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-    <name>WebRunWithNasar</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>3</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>true</rerunImmediately>
-    <testSuiteGuid>5be1da29-da5d-4894-ad33-f0a1249ed0cc</testSuiteGuid>
-    <testCaseLink>
-        <guid>a83b02bc-928b-43bd-b21e-853c966a6c7f</guid>
-        <isReuseDriver>false</isReuseDriver>
-        <isRun>true</isRun>
-        <testCaseId>Test Cases/Login/login</testCaseId>
-        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-    </testCaseLink>
+   <name>WebRunWithNasar</name>
+   <description></description>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5be1da29-da5d-4894-ad33-f0a1249ed0cc</testSuiteGuid>
+   <testCaseLink>
+      <guid>a83b02bc-928b-43bd-b21e-853c966a6c7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ced8e57d-74ec-40b6-b367-6f16de3c45e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Events/Create Event</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
