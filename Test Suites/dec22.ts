04541567aff4be9175dc87ec2d4f30ee181adf5f
subhan.hassan@globalnorthstar.com <katalon_g3_dummy_username>
2025-12-22 14:24:52 +0000

    Create "dec22" test suite

---
+++ Test Suites/dec22.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>dec22</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>c510f3d6-1438-4845-93d8-dde69f73eb7c</testSuiteGuid>
+    <testCaseLink>
+        <guid>ccffa25a-c033-4ab4-9cfd-1b5e91ac3d67</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Login/login</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+</TestSuiteEntity>
